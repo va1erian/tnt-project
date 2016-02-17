3b31fe17f15e712e2aa494fd31e8d11066d45b77
sebferrer <sebastien@sebferrer.fr>
2016-02-17 17:43:28 +0100

    Gestion des adresses
    template de la page : ok
    listAdresses au chargement de la page : ok
    checkAdress : ok

--- app/lib/services/addresses/addresses.js
+++ app/lib/services/addresses/addresses.js
@@ -6,7 +6,7 @@ exports.renderAddress = function(req, res) {
 };
 
 exports.checkAddress = function(req, res) {
-	//res.render('index', {title : "checkAdress"});
+	res.status(200).json({bool: true, formattedAddress: "8 avenue de la déchéance, 91420 Morangui", gps: {gpsLatitude:12.2,gpsLongitude:14.5}});
 };
 
 exports.addAddress = function(req, res) {
@@ -18,5 +18,5 @@ exports.deleteAddress = function(req, res) {
 };
 
 exports.getListAddresses = function(req, res) {
-	//res.render('index', {title : "getListAddresses"});
+	res.status(200).json([{idAddress: 1,number : 8,street: "Avenue de la déchéance",postalCode: "91420",city:"Morangui",country:"France",gps: {gpsLatitude:12.2,gpsLongitude:14.5}},{idAddress: 2,number : 14,street: "Rue du poulet",postalCode: "91600",city:"Savignou",country:"France",gps: {gpsLatitude:12.2,gpsLongitude:14.5}}]);
 };
