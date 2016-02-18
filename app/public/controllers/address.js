8c327ea497f0ae72dab3494b6e871120d6278c10
sebferrer <sebastien@sebferrer.fr>
2016-02-18 17:10:53 +0100

    Gestion adresses
    Toutes les routes Ajax prêtes

--- app/public/controllers/address.js
+++ app/public/controllers/address.js
@@ -2,18 +2,20 @@ var app = angular.module('tntApp', []);
 app.controller('addressCtrl', function($scope, $http)
 {
 	$scope.URL = "http://" + window.location.hostname + ":3000/tnt";
-
-	var url = $scope.URL + '/address/all';
 	
-    $http.get(url)
-        .success(function (data, status, headers, config) {
-       	 	$scope.addressesList = data;
-        })
-        .error(function (data, status, headers, config)
-        {
-            $scope.errorMessage = "SUBMIT ERROR";
-        });
+    $scope.listAddresses = function() {
+        var url = $scope.URL + '/address/all';
+        $http.get(url)
+            .success(function (data, status, headers, config) {
+           	 	$scope.addressesList = data;
+            })
+            .error(function (data, status, headers, config)
+            {
+                $scope.errorMessage = "SUBMIT ERROR";
+            });
+    }
 
+    $scope.listAddresses();
 
     $scope.checkAddress = function() {
         var url = $scope.URL + '/address/check';
@@ -22,6 +24,7 @@ app.controller('addressCtrl', function($scope, $http)
             .success(function (data, status, headers, config) {
                 if(data.success) {
                     $scope.confirmedAddress = data;
+                    $('#addNewAddress input').prop('disabled', true);
                     $('#validateAddress').show();
                 }
                 else {
@@ -35,11 +38,34 @@ app.controller('addressCtrl', function($scope, $http)
     }
 
     $scope.deleteAddress = function(id) {
-        var url = $scope.URL + '/address/delete?'+id;
+        var url = $scope.URL + '/address/delete?idAddress='+id;
 
         $http.get(url)
             .success(function (data, status, headers, config) {
-                $scope.addressesList = data;
+                if(data.success) {
+                    $scope.listAddresses();
+                }
+                else {
+
+                }
+            })
+            .error(function (data, status, headers, config)
+            {
+                $scope.errorMessage = "SUBMIT ERROR";
+            });
+    }
+
+    $scope.addAddress = function(formattedAddress) {
+        var url = $scope.URL + '/address/add?formattedAddress='+formattedAddress;
+
+        $http.get(url)
+            .success(function (data, status, headers, config) {
+                if(data.success) {
+                    $scope.listAddresses();
+                }
+                else {
+                    
+                }
             })
             .error(function (data, status, headers, config)
             {
