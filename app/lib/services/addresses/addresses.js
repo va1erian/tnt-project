02fcef1201f67926e990e446cecbbfc2fcf44675
yazidbousetta <yazid.bousetta@hlog-qostelecom.com>
2016-02-18 17:37:44 +0100

    adding wsRequest module

--- app/lib/services/addresses/addresses.js
+++ app/lib/services/addresses/addresses.js
@@ -2,10 +2,18 @@ var logger 	= require('../../log/logger');
 var request	= require('request');
 
 exports.renderAddress = function(req, res) {
-	res.render('address');
+	// redirect to the adresses view
+	// For the addresses list, when the view is completely charged, an ajax script will invoque
+	// the server to get the adresses list
+	res.render('address', {title : "Adresses"});
 };
 
 exports.checkAddress = function(req, res) {
+	
+	// request WS to check the address set and return the formatted address. The json by the returned : {success, formattedAddress, gps}
+	// return the same json as the ws's
+	
+	// If error : {success:false, error: '',}
 	res.status(200).json({success: true, formattedAddress: "8 Avenue du superbe Kiabi, Saumur, 91000", gps: {gpsLatitude:47.279127,gpsLongitude:-0.064639}});
 };
 
--- app/lib/utils/wsRequest.js
+++ app/lib/utils/wsRequest.js
@@ -7,8 +7,14 @@ exports.get = function(url, paramsJS, callback) {
 	}
 	myURL = myURL.substring(0, myURL.length - 1);
 	
-	
 	request.get({ url : myURL}, function(error, response, body) {
+		callback(error, response, body);
+	});
+};
+
+exports.post = function(url, dataJS, callback) {
+	var myURL = url;
+	request.get({ url : myURL, form : dataJS}, function(error, response, body) {
 		callback(error, response, body);
 	});
 };
