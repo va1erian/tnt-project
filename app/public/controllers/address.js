30469cf5d7b681782c2a4e17d767678d0a1b1671
sebferrer <sebastien@sebferrer.fr>
2016-02-19 15:11:01 +0100

    rAddd

--- app/public/controllers/address.js
+++ app/public/controllers/address.js
@@ -2,9 +2,21 @@ var app = angular.module('tntApp', []);
 app.controller('addressCtrl', function($scope, $http)
 {
 	$scope.URL = "http://" + window.location.hostname + ":3000/tnt";
+
+    $scope.new_address = {
+        name: '',
+        number: '',
+        street: '',
+        postalCode: '',
+        city: '',
+        country: '',
+        gpsLatitude: '',
+        gpsLongitude: '',
+        formattedAddress: ''
+    };
 	
     $scope.listAddresses = function() {
-        var url = $scope.URL + '/address/all';
+        var url = $scope.URL + '/address/list';
         $http.get(url)
             .success(function (data, status, headers, config) {
            	 	$scope.addressesList = data;
@@ -19,8 +31,7 @@ app.controller('addressCtrl', function($scope, $http)
 
     $scope.checkAddress = function() {
         var url = $scope.URL + '/address/check';
-
-        $http.post(url)
+        $http.post(url, $scope.new_address)
             .success(function (data, status, headers, config) {
                 if(data.success) {
                     $scope.foundAddresses = data.addresses;
@@ -40,6 +51,9 @@ app.controller('addressCtrl', function($scope, $http)
 
     $scope.confirmAddress = function(addr) {
         $scope.confirmedAddress = addr;
+        $scope.new_address.formattedAddress = addr.formattedAddress;
+        $scope.new_address.gpsLatitude = addr.gps.gpsLatitude;
+        $scope.new_address.gpsLongitude = addr.gps.gpsLongitude;
         $('#addr_validationBlock_list').fadeOut(200, function() {
             $('#addr_validationBlock_map').fadeIn(200);
         });
@@ -66,9 +80,11 @@ app.controller('addressCtrl', function($scope, $http)
     $scope.addAddress = function(formattedAddress) {
         var url = $scope.URL + '/address/add';
 
-        $http.post(url)
+        $http.post(url, $scope.new_address)
             .success(function (data, status, headers, config) {
                 if(data.success) {
+                    console.log("Address sent : ");
+                    console.log($scope.new_address);
                     $scope.listAddresses();
                     $('#addr_validationBlock_map').fadeOut(200, function() {
                         $scope.confirmedAddress = '';
